c110e03c5706cd33a3e09951f74ee77046d27ae4
Andreas <andreas.kielsgaard@gmail.com>
2018-07-16 17:00:55 +0200

    Forgot to add this

--- Automation/src/halcyon/robouser/com/gui/elements/RoutineView.java
+++ Automation/src/halcyon/robouser/com/gui/elements/RoutineView.java
@@ -1,38 +1,64 @@
-package halcyon.robouser.com;
+package halcyon.robouser.com.gui.elements;
 
 import java.awt.Color;
-import java.awt.GridBagConstraints;
+import java.awt.Dimension;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import halcyon.robouser.com.Utility;
+import halcyon.robouser.com.actionEngine.Action;
 
 //Contains gui elements: A list of actions to be performed during this routine, newAction button,
 //  actions contain their own buttons for modifying. 
 public class RoutineView extends JPanel {
 	
-	JList<ActionView> actionList;
+//	JList<ActionView> actionList;
+	ArrayList<ActionView> actionList;
 	JButton addAction;
+	Action action;
+	
+	JScrollPane listView;
+	JPanel listPanel;
 	
 	public RoutineView() {
-		
+		setLayout(new GridBagLayout());
 		
 		//Initialize components 
-		actionList = new JList<ActionView>();
+		actionList = new ArrayList<ActionView>();
 		addAction = new JButton("Add Action");
+		listPanel = new JPanel();
+		listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.PAGE_AXIS));
+		listView = new JScrollPane(listPanel);
+		listView.setPreferredSize(new Dimension(500, 500));
 		
 		//Add components
-		Utility.addToPanelAt(this, actionList, 0, 0);
+		Utility.addToPanelAt(this, listView, 0, 0);
 		Utility.addToPanelAt(this, addAction, 0, 1);
+		
+		//Map methods to buttons
+		addAction.addActionListener(e -> {addAction();});
 		
 		setBackground(Color.LIGHT_GRAY);
 
-		addAction.addActionListener(e -> {addAction();});
 	}
 	
 	private void addAction() {
+		AddActionDialogue dialogue = new AddActionDialogue();
+		int result = JOptionPane.showConfirmDialog(null, dialogue, "Add Action",
+				JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+		
+		action = dialogue.getAction();
+		actionList.add(new ActionView(action));
+		listPanel.add(actionList.get(actionList.size() - 1));
+		
+		validate();
+		repaint();
 		
 	}
 	
